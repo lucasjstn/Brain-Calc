ac0aca78611a03c6a18ceb2e6860280ae99c561d
lucasjstn <lucas7justino@gmail.com>
2022-09-28 01:46:00 -0300

    intentional error

--- public/main.js
+++ public/main.js
@@ -6,3 +6,10 @@ array = [
     ["another string", 23],
 ];
 console.log(array);
+//later reassign
+//this will cause an error because the extra number element is not declared in the tuple
+array = [
+    ["string", 39283],
+    ["another string", 23],
+    ["string", 23, 239],
+];
