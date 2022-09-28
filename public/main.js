9b3c5e91bbd77e80ff2157a9348e96e47cb9e1bc
lucasjstn <lucas7justino@gmail.com>
2022-09-28 01:13:34 -0300

    learning typescript

---
+++ public/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let person = [];
+let number = 29;
+person = ["Lucas", number];
+console.log(person);
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+let person: (string | number)[] = [];
+let number: number = 29;
+
+person = ["Lucas", number];
+
+console.log(person);
