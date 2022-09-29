181c4f0970b59195e6243206fb3fac47a2af2b03
lucasjstn <lucas7justino@gmail.com>
2022-09-29 14:02:26 -0300

    inverting the display order because it's buggy by default

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@
 const display = document.getElementById("display"); //declared as any to get rid of the error alert
 console.log(display);
 function writeOnDisplay(item) {
-    display.value += item;
+    display.value = item + display.value;
 }
 function clearDisplay() {
     display.value = "";
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-
 const display: any = document.getElementById("display"); //declared as any to get rid of the error alert
 
 console.log(display);
 
 function writeOnDisplay(item?: string) {
-  display.value += item;
+  display.value = item + display.value;
 }
 
 function clearDisplay() {
