a735a5686db53aa79c119ff709aa25c9c8a6b092
lucasjstn <lucas7justino@gmail.com>
2023-01-03 17:06:28 -0300

    making a switch case for operators and numbers

--- public/post-fix-evaluator.js
+++ public/post-fix-evaluator.js
@@ -39,5 +39,32 @@ const EXPRESSION = [
   "*",
   "-",
 ];
+const isNumber = /[0-9]/;
+const isOperator = /[+*-/^]/;
+// console.log(isNumber.test(2));
 
-console.log(EXPRESSION);
+//List of Characters -> Number
+function Evaluating(exp) {
+  //creates the evaluating stack
+  let evalStack = new Stack();
+
+  //if the given list is empty the sum will be equal to 0
+  if (exp.length === 0) return 0;
+  //for each element inside an array make a process
+  for (let i = 0; i < exp.length; i++) {
+    // console.log(exp[i], i);
+    // console.log();
+    switch (true) {
+      //tests if its a number
+      case isNumber.test(exp[i]):
+        console.log("number");
+        break;
+      // tests if its a operator
+      case isOperator.test(exp[i]):
+        console.log("operator");
+        break;
+    }
+  }
+}
+
+console.log(Evaluating(EXPRESSION));
