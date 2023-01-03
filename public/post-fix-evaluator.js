9860e2e52310e196e7f56fa57e5cb3606dbcae9e
lucasjstn <lucas7justino@gmail.com>
2023-01-03 18:40:03 -0300

    postfix evaluation done

--- public/post-fix-evaluator.js
+++ public/post-fix-evaluator.js
@@ -7,8 +7,7 @@ class Stack {
     this.items.push(item);
   }
   pop() {
-    if (!this.isEmpty()) return this.items.pop;
-    return "Underflow";
+    this.items.pop();
   }
 
   isEmpty() {
@@ -18,6 +17,16 @@ class Stack {
   size() {
     return this.items.length;
   }
+
+  last() {
+    return Number(this.items[this.size() - 1]);
+  }
+
+  penultimate() {
+    return Number(this.items[this.size() - 2]);
+  }
+
+  removeFirst() {}
 }
 
 const EXPRESSION = [
@@ -47,24 +56,58 @@ const isOperator = /[+*-/^]/;
 function Evaluating(exp) {
   //creates the evaluating stack
   let evalStack = new Stack();
-
+  let lastResult = 0;
   //if the given list is empty the sum will be equal to 0
   if (exp.length === 0) return 0;
   //for each element inside an array make a process
   for (let i = 0; i < exp.length; i++) {
-    // console.log(exp[i], i);
-    // console.log();
+    let result = 0;
     switch (true) {
       //tests if its a number
       case isNumber.test(exp[i]):
-        console.log("number");
+        // console.log("number");
+        evalStack.push(exp[i]);
+        // console.log(evalStack.items);
         break;
       // tests if its a operator
       case isOperator.test(exp[i]):
-        console.log("operator");
+        // console.log("operator");
+        result = operations[exp[i]](evalStack.penultimate(), evalStack.last());
+        // console.log(
+        //   operations[exp[i]](evalStack.penultimate(), evalStack.last())
+        // );
+        evalStack.pop();
+        evalStack.pop();
+        evalStack.push(result);
+        // evalStack.pop();
+        // console.log(evalStack.pop());
         break;
+    }
+
+    if (i == exp.length - 1) {
+      return result;
     }
   }
 }
 
+const operations = {
+  "+": (number1, number2) => {
+    return number1 + number2;
+  },
+  "-": (number1, number2) => {
+    return number1 - number2;
+  },
+  "*": (number1, number2) => {
+    return number1 * number2;
+  },
+  "/": (number1, number2) => {
+    return number1 / number2;
+  },
+  "^": (number1, number2) => {
+    return Math.pow(number1, number2);
+  },
+};
+
+// console.log(Math.pow(3, 2));
+// console.log(operations["+"](2, 3));
 console.log(Evaluating(EXPRESSION));
